dc9a44c7eef2ad3f7f6b4771306007a60e9fa100
Paul Peyrard <paul.peyrard17@gmail.com>
2023-03-13 14:39:35 +0100

    change port modifiable en fct du serveur render qui peut modifier le port

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000;  //Si sur le serveur le port existe alors ça prend le port donné, sinon ça prend 3000
 
+app.get("/toto", function(req, res) {
+    res.send("salut toto")
+});
 
 app.listen(port, function() {
     console.log('serveur listening on port : '+port)
-})
+});
