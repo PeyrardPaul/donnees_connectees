42a46d71748f7b988509a7766450d277174f00b5
Paul Peyrard <paul.peyrard17@gmail.com>
2023-03-13 15:19:56 +0100

    formulaire

--- index.js
+++ index.js
@@ -3,9 +3,17 @@ var app = express();
 
 const port = process.env.PORT || 3000;  //Si sur le serveur le port existe alors ça prend le port donné, sinon ça prend 3000
 
-app.get("/toto", function(req, res) {
-    res.send("salut toto")
-});
+app.use(express.urlencoded({ extended: true }))
+
+app.post('/home', (req, res) => {
+  const name = req.body.name
+  console.log(`L'annotation saisie est ${name}`)
+  res.send(`L'annotation saisie est ${name}`)
+})
+
+// app.get("/toto", function(req, res) {
+//     res.send("salut toto")
+// });
 
 app.listen(port, function() {
     console.log('serveur listening on port : '+port)
