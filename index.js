259b97f9032f590af07a43cf7f46221078b04dda
Paul Peyrard <paul.peyrard17@gmail.com>
2023-03-14 14:16:32 +0100

    bien avancé, recup 1 annot ok, créer 1 annot ok, tout afficher ok, manque

--- index.js
+++ index.js
@@ -1,72 +1,115 @@
 var express = require('express');
 var app = express();
-var notes = [];
-var Id = 0;
+var data = {};
+var id = 0;
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
 const port = process.env.PORT || 3000;  //Si sur le serveur le port existe alors ça prend le port donné, sinon ça prend 3000
 
 app.use(express.urlencoded({ extended: true }))
+app.use(express.static('html'));
 
-
-// route pour afficher le formulaire
-// app.get('/formulaire', (req, res) => {
-//   res.send(`
-//     <form action="/result" method="post">
-//         <label for="note">Annotation:</label>
-//         <input type="number" id="note" name="note">
-
-//         <label for="com">Commentaire:</label>
-//         <input type="text" id="com" name="com">
-
-//         <button type="submit">Envoyer</button>
-//     </form>
-//     `)
-// })
 
 // route pour traiter les données du formulaire
 app.get("/formulaire", function(req, res) {
     res.sendfile("index.html");
-})
+});
+
+app.post("/formulaire", function(req, res){
+	var body = req.body;
+	data[id] = body					
+	res.send("votre annotation a été sauvegardé à l'identifiant "+id+" !");
+    id++;
+    console.log(data)
+});
 
 
-app.post('/annotation', (req, res) => {
-    const note = req.body.note
-    const commentaire = req.body.com
-    // res.send(`
-    //     Si vous voulez voir toutes les annotations: <form action="/resultall" method="get"><button type="submit">Cliquez-ici</button></form>
-    //     <br><br>
-    //     <form action="/formulaire" method="get">
-    //         <button type="submit">Cliquez-ici
-    //         </button>
-    //     </form>
-    //     `)
-    res.redirect("/annotation");
-    notes.push({"Id": Id, "Annotation": `${note}`, "Commentaire":`${commentaire}`});
-    Id += 1;
-    console.log(notes[Id]["Annotation"],"ddd",notes["Annotation"],"ettt",notes[0]["Annotation"])
-  })
+//Reçoit le formulaire et redirige vers une page  avec comme route l'identifiant
+app.get("/annotation", function(req, res){
+    const id = req.query.IdAnnot
+    res.redirect(`/annotation/${id}`)
+});
 
-  app.get('/annotation', (req, res) => {
-    res.send(`Votre annotation a été sauvegardé ! <br>
-        Si vous voulez voir toutes les annotations: 
-        <form action="/resultall" method="get">
-            <button type="submit">Cliquez-ici</button>
-        </form>
-        <br><br>
-        <form action="/formulaire" method="get">
-            <button type="submit">Accueil</button>
-        </form>`)
-  })
+//lance la page de l'annotation correspondante
+app.get("/annotation/:IdAnnot", function(req, res){
+    const id = req.params.IdAnnot // Récupérer l'identifiant de l'utilisateur depuis la requête POST    
+    
+    var ChoixFormat=req.query.FormatIdAnnot;
+	if (ChoixFormat=="html"){
+		res.set('Content-Type', 'text/html');
+	}
+	else {
+		if (ChoixFormat=="Json"){
+            res.set('Content-Type', 'application/json');
+		}	
+	}
 
-app.get('/resultall', (req, res) => {
+    var Exist=Object.keys(data).includes(id);
+    res.format ({
+		   'text/html': function() {
+			    if (Exist){
+				   res.send(data[id]); 
+			    }
+			    else {
+				   res.send("Aucune annotation n'est associée à cette clé");
+			    }
+		   },
+
+		   'application/json': function() {
+			    if (Exist){
+				   res.send(data[id]); 
+			    }
+			    else {
+				   res.send("Aucune annotation n'est associée à cette clé");
+			    }
+			}
+	});
+	
+});
+
+
+
+// app.post('/annotation', (req, res) => {
+//     const note = req.body.note
+//     const commentaire = req.body.com
+//     res.redirect("/annotation");
+//     notes.push({"Id": Id, "Annotation": `${note}`, "Commentaire":`${commentaire}`});
+//     Id += 1;
+//   })
+
+//   app.get('/annotation', (req, res) => {
+//     res.send(`Votre annotation a été sauvegardé ! <br>
+//         Si vous voulez voir toutes les annotations: 
+//         <form action="/resultall" method="get">
+//             <button type="submit">Cliquez-ici</button>
+//         </form>
+//         <br><br>
+//         <form action="/formulaire" method="get">
+//             <button type="submit">Accueil</button>
+//         </form>`)
+//   })
+
+app.post('/recupAll', (req, res) => {
+    var ChoixFormat=req.query.FormatIdAnnot;
+    if (ChoixFormat=="html"){
+		res.set('Content-Type', 'text/html');
+	}
+	else {
+		if (ChoixFormat=="Json"){
+            res.set('Content-Type', 'application/json');
+		}	
+	}
+
     let html = '<ul>';
 
-    for (let i = 0; i < notes.length; i++) {
-      html += '<li>Note=' + notes[Id]["Annotation"] + '    et commentaire=' + notes[Id]["Commentaire"] +'</li>';
+    for (let key in data) {
+      html += '<li>Identifiant=' + key +'  Note='+ data[key].Note +'  et Commentaire='+ data[key].Commentaire +'</li>';
     }
-  
     html += '</ul>';
     res.send(html);
+    console.log(html)
 })
 
 
